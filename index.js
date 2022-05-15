2121d36bc96a39d6bbf521602e7e4f9d27e0d2ec
kisnaa0709 <104568743+kisnaa0709@users.noreply.github.com>
2022-05-15 16:09:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+       icon.src = "img/sun1.png";
+    }
+    else{
+        icon.src = "img/moon.png";
+    }
+
+}
