4222b2a60646dbf50f79e4ae80fb37cd94aaf4ba
Kishan Singh <104568743+kisnaa0709@users.noreply.github.com>
2022-05-25 00:10:31 +0530

    Sixth commit

--- index.js
+++ index.js
@@ -30,6 +30,20 @@ function loginu() {
     alert("Run away You are not Safe "+input);
     }
 
+
+
+let liked = document.getElementById("likee")
+let likeu = 3000
+
+function like(){
+    likeu += 1
+    liked.innerText = likeu + (" views")
+   
+}
+
+
+
+
 function createu() {
     var input = document.getElementById("createuser").value;
     alert("There is No Space For You " +input+ " Get Lost");
@@ -44,3 +58,6 @@ function createu() {
 $('.creates a') .click(function(){
 $('form').animate({height:"toggle", opacity: "toggle"}, "slow")
 })
+
+
+
