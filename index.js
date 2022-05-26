6bd743fc07e6ceeac3f205b147c64ac9ea914a79
Kishan Singh <104568743+kisnaa0709@users.noreply.github.com>
2022-05-26 17:16:53 +0530

    Sixth

--- index.js
+++ index.js
@@ -25,36 +25,52 @@ click.addEventListener("click",()=>{
 })
 
 
+let liked1 = document.getElementById("likee1")
+let liked2 = document.getElementById("likee2")
+let liked3 = document.getElementById("likee3")
+let liked4 = document.getElementById("likee4")
+let liked5 = document.getElementById("likee5")
+let liked6 = document.getElementById("likee6")
+
+function like1(){
+    liked1.textContent = parseInt(liked1.textContent) + 1
+    liked1.innerText = liked1.textContent + (" ") + ("views") 
+}
+function like2(){
+    liked2.textContent = parseInt(liked2.textContent) + 1
+    liked2.innerText = liked2.textContent + (" ") + ("views")
+}
+function like3(){
+    liked3.textContent = parseInt(liked3.textContent) + 1
+    liked3.innerText = liked3.textContent + (" ") + ("views")   
+}
+function like4(){
+    liked4.textContent = parseInt(liked4.textContent) + 1
+    liked4.innerText = liked4.textContent + (" ") + ("views")   
+}
+function like5(){
+    liked5.textContent = parseInt(liked5.textContent) + 1
+    liked5.innerText = liked5.textContent + (" ") + ("views")   
+}
+function like6(){
+    liked6.textContent = parseInt(liked6.textContent) + 1
+    liked6.innerText = liked6.textContent + (" ") + ("views")   
+}
+
+
+
 function loginu() {
     var input = document.getElementById("loginuser").value;
     alert("Run away You are not Safe "+input);
     }
 
 
-
-let liked = document.getElementById("likee")
-let likeu = 3000
-
-function like(){
-    likeu += 1
-    liked.innerText = likeu + (" views")
-   
-}
-
-
-
-
 function createu() {
     var input = document.getElementById("createuser").value;
     alert("There is No Space For You " +input+ " Get Lost");
     }
-// body.onclose("click",()=>{
-//     if (login.style.display === "block") {
-//          login.style.display = "none"
 
-//     }
-   
-// })
+
 $('.creates a') .click(function(){
 $('form').animate({height:"toggle", opacity: "toggle"}, "slow")
 })
